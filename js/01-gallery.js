de0a82b42b80d130fd7468c9aaf0f87e34edb5cb
Praded3 <pradid.store@gmail.com>
2023-07-12 02:06:27 +0300

    start

---
+++ js/01-gallery.js
@@ -0,0 +1,48 @@
+import { galleryItems } from './gallery-items.js';
+
+const list = document.querySelector('.gallery');
+
+const itemsMarkup = galleryItems.map((item) => {
+    return `<li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+            <img
+                class="gallery__image"
+                src="${item.preview}"
+                data-source="${item.original}"
+                alt="${item.description}"
+            />
+        </a>
+    </li>`;
+}).join("");
+
+list.innerHTML = itemsMarkup;
+
+const onImgClick = (e) => {
+    
+    e.preventDefault();
+    console.log('e', e)
+    
+    if (e.target.nodeName === 'IMG'){ 
+        const instance  = basicLightbox.create(`
+            <img src="${e.target.dataset.source}">
+        `,
+        {
+            onShow: (instance ) => {
+                document.addEventListener("keydown", onEscapeKeydowm);
+            },
+            onClose: (instance ) => {
+                document.removeEventListener("keydown", onEscapeKeydowm);
+            },
+        }
+        );
+        instance.show();
+
+        function onEscapeKeydowm(event) {
+            if (event.code === "Escape") {
+                instance.close();
+            }
+        };
+    };
+};
+
+list.addEventListener('click', onImgClick);
---
+++ js/02-lightbox.js
@@ -0,0 +1,21 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+const list = document.querySelector('.gallery');
+
+const itemsMarkup = galleryItems.map((item) => {
+    return `
+        <li class="gallery__item">
+            <a class="gallery__link" href=${item.original}>
+                <img class="gallery__image" src=${item.preview} alt=${item.description} />
+            </a>
+        </li>`;
+}).join("");
+
+list.innerHTML = itemsMarkup;
+
+var lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt',
+    captionDelay: 250,
+});
